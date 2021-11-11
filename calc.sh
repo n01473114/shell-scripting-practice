5f5250493f0b08178167413fdbf32ee8cffe886b
Jose Sandoval <n01473114@humbermail.ca>
2021-11-10 21:34:04 -0500

    Style for the script.

--- calc.sh
+++ calc.sh
@@ -2,17 +2,21 @@
 
 # VARIABLES
 quit=n
+separator="_______________"
+
+# WELCOME
+echo "HELLO! WELCOME TO CALCAPP" # When the program is first loaded, display a greeting to the user.
+echo
 
 # When the operation is complete, redisplay the menu.
 while [[ $quit = "n" ]]
 	do
-
-		echo "HELLO! WELCOME TO CALCAPP" # When the program is first loaded, display a greeting to the user.
 		echo "Please select one of the following actions:" # Then, display a menu that outlines the possible operations:
 		echo [1] Add # Add
 		echo [2] Subtract # Subtract
 		echo [3] Exit  # Exit
 		read ops # Then, capture the user selection.
+		echo
 
 		# If the selection matches a supported operation, execute the operation.
 		# If the selection does not match a support operation, display an error message.
@@ -23,8 +27,11 @@ while [[ $quit = "n" ]]
 				echo "Please provide the second value to add:"
 				read value2
 				let total="$value1"+"$value2"
-				echo "Answer:"
-				echo $total
+				echo
+				echo -e "\e[7mAnswer:\e[0m"
+            	echo $total
+            	echo $separator
+            	echo
 			;;
 			2)
 				echo "Please provide a first value:"
@@ -32,16 +39,21 @@ while [[ $quit = "n" ]]
 				echo "Please provide the second value to subtract:"
 				read value2
 				let total="$value1"-"$value2"
-				echo "Answer:"
+				echo
+				echo -e "\e[7mAnswer:\e[0m"
 				echo $total
+				echo $separator
+				echo
 			;;
 			3)
 				echo "Are you sure you want to exit? [Y/N]"
 				read quit
+				echo $separator
+            	echo
 			;;
 			*)
-				invalid="$ops"
-				echo "*** $invalid is not a valid option ***"
+				invalid="\e[33m$ops\e[0m\e[1;31m"
+            	echo -e "\e[1;31m *** $invalid is not a valid option *** \e[0m" && echo
 			;;
 		esac
-	done
+done
