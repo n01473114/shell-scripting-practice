dae366a1cb4793dba62761e3c7ae0592f4486cdf
Jose Sandoval <n01473114@humbermail.ca>
2021-11-10 21:10:54 -0500

    Menu with possible operations.

--- calc.sh
+++ calc.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
 echo "HELLO! WELCOME TO CALCAPP" # When the program is first loaded, display a greeting to the user.
-# Then, display a menu that outlines the possible operations:
-  # Add
-  # Subtract
-  # Exit
+echo "Please select one of the following actions:" # Then, display a menu that outlines the possible operations:
+echo [1] Add # Add
+echo [2] Subtract # Subtract
+echo [3] Exit  # Exit
+
 # Then, capture the user selection.
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
