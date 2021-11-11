8d14f487ec3c0228fc1f64c5d94cae5539370f41
Jose Sandoval <n01473114@humbermail.ca>
2021-11-10 21:16:03 -0500

    Capture the user selection.

--- calc.sh
+++ calc.sh
@@ -5,8 +5,7 @@ echo "Please select one of the following actions:" # Then, display a menu that o
 echo [1] Add # Add
 echo [2] Subtract # Subtract
 echo [3] Exit  # Exit
-
-# Then, capture the user selection.
+read ops # Then, capture the user selection.
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
